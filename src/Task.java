1559d92a3b3cdce873e62b91a417375218128e1e
josh2180 <josh2180@ISS147D8-15.scdsb.pri>
2016-01-21 11:06:20 -0500

    dtyfjftjfjfjtff

---
+++ src/Task.java
@@ -0,0 +1,35 @@
+
+public class Task {
+
+    String name;
+    String description;
+
+    public Task(String n, String d) {
+        name = n;
+        description = d;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDesc() {
+        return description;
+    }
+    
+//    public String setName(){
+//        
+//    }
+
+    public boolean validate() {
+        if (name == null || description == null || name.equals("") || description.equals("")) {
+            return false;
+        } else {
+            return true;
+        }
+    }
+
+    public String toString() {
+        return "Name: " + name + "\nDescription: " + description + "\n========\n";
+    }
+}
